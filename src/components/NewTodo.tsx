6ea0bc6e61234a50169fa7a24aaa5b3aefbc2532
spatulatom <spatulatom@gmail.com>
2021-06-19 17:31:56 +0100

    first commit

---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,51 @@
+import React, { useContext, useState, ReactEventHandler } from 'react';
+
+import { TodosContext } from '../store/todos-context';
+import classes from './NewTodo.module.css';
+
+const NewTodo: React.FC = () => {
+const [todo, setTodo] = useState("")
+
+const handleChange = (e: React.ChangeEvent<HTMLInputElement>)=>
+{
+ return  setTodo(e.target.value);
+};
+
+
+const todosCtx = useContext(TodosContext);
+const submitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+
+  
+
+    // something I missing here, the check for the value entered
+    // is only here so it means that the possobility of it being zero
+    // which basucally is nul exist up to this pointso it must have passed 
+    // the code above where exclamation mark prevents the value from being
+    // null, 
+    // unless that prevents empty spaceces which tehnicalyy are not null
+    // and what acually prevents that value from being null is the <form>
+    // itself 
+
+    if (todo.trim().length === 0) {
+      // throw an error
+      return;
+    }
+
+    todosCtx.addTodo(todo);
+    setTodo("")
+    
+    
+  };
+
+  return (
+    <form onSubmit={submitHandler} className={classes.form}>
+      {/* <form className={classes.form}> */}
+            <label htmlFor='text'>Todo in React & TypeScript</label>
+      <input type='text' id='text' value={todo} onChange={handleChange} />
+      <button>Add Todo</button>
+    </form>
+  );
+};
+
+export default NewTodo;
---
+++ src/store/todos-context.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+
+import Todo from '../models/todo';
+
+type TodosContextObj = {
+  items: Todo[];
+  addTodo: (text: string) => void;
+  removeTodo: (id: string) => void;
+};
+
+// we eksporting TodoContext, why? apparently we need that for  use context later
+export const TodosContext= React.createContext<TodosContextObj>({
+  // this is just pure JS her yet decribing function is somewhat new
+  // like why there is nothing there in curly brackets
+  items: [],
+  addTodo: (text:string) => {},
+  removeTodo: (id: string) => {},
+});
+
+const TodosContextProvider: React.FC = (props) => {
+  // we only have props.children and they default and therfore
+  // no need to define them here: React.FC<no need>
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    });
+  };
+
+  const removeTodoHandler = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== todoId);
+    });
+  };
+
+  const contextValue: TodosContextObj = {
+    items: todos,
+    addTodo: addTodoHandler,
+    removeTodo: removeTodoHandler,
+  };
+
+  return (
+    <TodosContext.Provider value={contextValue}>
+      {props.children}
+    </TodosContext.Provider>
+  );
+};
+
+ export default TodosContextProvider;
